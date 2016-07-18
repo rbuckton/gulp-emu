a198113609c91e6c90cc5a31b15082857d3f5c5d
Ron Buckton <rbuckton@chronicles.org>
2016-07-18 11:23:21 -0700

    Added typings to package and updated prepublish script.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,21 @@ var gulp = require("gulp")
   , del = require("del");
 
 var project = tsb.create("src");
+var release = project.withCompilerOptions({ sourceMap: false });
 
 gulp.task("clean", cb => del(["out"], cb));
 
-gulp.task("build", () => project
+gulp.task("build:debug", () => project
     .src()
     .pipe(project.compile())
     .pipe(gulp.dest("out")));
+
+gulp.task("build:release", ["clean"], () => release
+    .src()
+    .pipe(release.compile())
+    .pipe(gulp.dest("out")));
+
+gulp.task("build", ["build:release"]);
 
 gulp.task("accept-baselines", () => gulp
     .src("tests/baselines/local/**/*")
@@ -19,13 +27,13 @@ gulp.task("accept-baselines", () => gulp
 gulp.task("clean-local-baselines", cb => del(["tests/baselines/local"])
     .then(() => cb, e => cb(e)));
 
-gulp.task("test", ["build", "clean-local-baselines"], () => gulp
+gulp.task("test", ["build:debug", "clean-local-baselines"], () => gulp
     .src("out/tests/index.js", { read: false })
     .pipe(mocha()));
 
-gulp.task("watch", ["build"], () => gulp
+gulp.task("watch", ["build:debug"], () => gulp
     .watch(project.globs.concat(["tests/baselines/reference/**/*", "tests/scenarios/**/*"]), ["test"]));
 
-gulp.task("prepublish", ["test"]);
+gulp.task("prepublish", ["build:release"]);
 
-gulp.task("default", ["build"]);
+gulp.task("default", ["test"]);
